c40ef228fc406aff03ed42d5bd5391a125fe01c8
Benoit-78 <benoit.delorme@gadz.org>
2023-01-21 10:07:43 -0500

    feat: list-accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -158,3 +158,10 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_list_accounts(self):
+        self._create_accounts(5)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 5)
